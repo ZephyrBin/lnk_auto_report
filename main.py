f21f117e83f9ea44d842c382bbd319a9e19b4f53
EDZephyr <103563317+EDZephyr@users.noreply.github.com>
2024-10-27 20:52:37 +0900

    useless parse deleted

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from analyze_lnk import LNKAnalyzer
 def main():
     parser = argparse.ArgumentParser(description='LNK File Analyzer')
     parser.add_argument('lnk_path', help='Path to the LNK file')
-    parser.add_argument('--vt-api-key', help='VirusTotal API key')
     args = parser.parse_args()
 
     analyzer = LNKAnalyzer(args.lnk_path, VT_API)
